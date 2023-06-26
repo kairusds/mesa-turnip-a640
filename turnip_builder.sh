21d7325abcc68db41d0b056b56f1d4bf44232bb0
kairusds <harveyhansjavier@gmail.com>
2023-06-26 23:18:24 +0800

    Update turnip_builder.sh
    - Removed colors
    - Use Android NDK provided by Actions
    - Set action environment variable for MESA_COMMIT_HASH
    - Set output driver's minimum SDK version to Android 30
    - Dont rename libvulkan_freedreno.so to vukan.adreno.so
    - Only require libvulkan_freedreno.so and libbacktrace.so for the driver zip file
    - Updated driver metadata(meta.json) to reflect changes
    - And some minor changes

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
-green='\033[0;32m'
-red='\033[0;31m'
 nocolor='\033[0m'
 deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 driverdir="$workdir/turnip_module"
-ndkver="android-ndk-r25c"
 clear
 
-
-
-echo "Checking system for required Dependencies ..."
+echo "Checking system for required dependencies..."
 for deps_chk in $deps;
 	do 
 		sleep 0.25
 		if command -v $deps_chk >/dev/null 2>&1 ; then
-			echo -e "$green - $deps_chk found $nocolor"
+			echo -e "$deps_chk found"
 		else
-			echo -e "$red - $deps_chk not found, can't countinue. $nocolor"
+			echo -e "$deps_chk not found, can't countinue."
 			deps_missing=1
 		fi;
 	done
@@ -26,42 +21,26 @@ for deps_chk in $deps;
 		then echo "Please install missing dependencies" && exit 1
 	fi
 
-
-
 echo "Installing python Mako dependency (if missing) ..." $'\n'
 pip install mako &> /dev/null
-
-
 
 echo "Creating and entering to work directory ..." $'\n'
 mkdir -p $workdir && cd $workdir
 
-
-
-echo "Downloading android-ndk from google server (~506 MB) ..." $'\n'
-curl https://dl.google.com/android/repository/"$ndkver"-linux.zip --output "$ndkver"-linux.zip &> /dev/null
+# echo "Downloading mesa source (~30 MB) ..." $'\n'
+# curl https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip --output mesa-main.zip &> /dev/null
 ###
-echo "Exracting android-ndk to a folder ..." $'\n'
-unzip "$ndkver"-linux.zip  &> /dev/null
-
-
-
-echo "Downloading mesa source (~30 MB) ..." $'\n'
-curl https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip --output mesa-main.zip &> /dev/null
-###
-echo "Exracting mesa source to a folder ..." $'\n'
-unzip mesa-main.zip &> /dev/null
+# echo "Exracting mesa source to a folder ..." $'\n'
+# unzip mesa-main.zip &> /dev/null
 cd mesa-main
 
-
-
 echo "Creating meson cross file ..." $'\n'
-ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
+ndk="$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin"
 cat <<EOF >"android-aarch64"
 [binaries]
 ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android31-clang']
-cpp = ['ccache', '$ndk/aarch64-linux-android31-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++']
+c = ['ccache', '$ndk/aarch64-linux-android30-clang']
+cpp = ['ccache', '$ndk/aarch64-linux-android30-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++']
 c_ld = 'lld'
 cpp_ld = 'lld'
 strip = '$ndk/aarch64-linux-android-strip'
@@ -73,66 +52,52 @@ cpu = 'armv8'
 endian = 'little'
 EOF
 
-
-
 echo "Generating build files ..." $'\n'
-meson build-android-aarch64 --cross-file $workdir/mesa-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=31 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> $workdir/meson_log
-
-
-
+meson build-android-aarch64 --cross-file $workdir/mesa-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=30 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> $workdir/meson_log
 echo "Compiling build files ..." $'\n'
 ninja -C build-android-aarch64 &> $workdir/ninja_log
-
-
-
-echo "Using patchelf to match soname ..."  $'\n'
+# echo "Using patchelf to match soname ..."  $'\n'
 cp $workdir/mesa-main/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so $workdir
-cp $workdir/mesa-main/build-android-aarch64/src/android_stub/libhardware.so $workdir
-cp $workdir/mesa-main/build-android-aarch64/src/android_stub/libsync.so $workdir
+# cp $workdir/mesa-main/build-android-aarch64/src/android_stub/libhardware.so $workdir
+# cp $workdir/mesa-main/build-android-aarch64/src/android_stub/libsync.so $workdir
 cp $workdir/mesa-main/build-android-aarch64/src/android_stub/libbacktrace.so $workdir
 cd $workdir
-patchelf --set-soname vulkan.adreno.so libvulkan_freedreno.so
-mv libvulkan_freedreno.so vulkan.adreno.so
 
+# patchelf --set-soname vulkan.adreno.so libvulkan_freedreno.so
+# mv libvulkan_freedreno.so vulkan.adreno.so
 
-
-if ! [ -a vulkan.adreno.so ]; then
-	echo -e "$red Build failed! $nocolor" && exit 1
+if ! [ -a libvulkan_freedreno.so ]; then
+	echo -e "Build failed!" && exit 1
 fi
 
-
-
-echo "Prepare magisk module structure ..." $'\n'
+echo "Creating driver metadata..." $'\n'
 mkdir -p $driverdir
 cd $driverdir
-
 
 cat <<EOF >"meta.json"
 {
   "schemaVersion": 1,
-  "name": "Mesa Turnip Adreno Driver 23.2.0",
-  "description": "Open-source Vulkan driver build from mesa drivers repo",
-  "author": "Mr_Purple_666",
-  "packageVersion": "T-Alpha",
+  "name": "Mesa3D Turnip Driver",
+  "description": "Open-source Vulkan driver for Adreno 640",
+  "author": "kairusds",
+  "packageVersion": "git",
   "vendor": "Mesa",
-  "driverVersion": "23.2.0-devel",
+  "driverVersion": "$MESA_COMMIT_HASH",
   "minApi": 30,
-  "libraryName": "vulkan.adreno.so"
+  "libraryName": "libvulkan_freedreno.so"
 }
 EOF
 
-
-echo "Copy necessary files from work directory ..." $'\n'
-cp $workdir/vulkan.adreno.so $driverdir
-cp $workdir/libhardware.so $driverdir
-cp $workdir/libsync.so $driverdir
+echo "Copying necessary files to the driver directory..." $'\n'
+cp $workdir/libvulkan_freedreno.so $driverdir
+# cp $workdir/libhardware.so $driverdir
+# cp $workdir/libsync.so $driverdir
 cp $workdir/libbacktrace.so $driverdir
 
-
-
-echo "Packing files in to magisk module ..." $'\n'
-zip -r $workdir/turnip-23.2.0-T-Alpha_MrPurple.adpkg.zip * &> /dev/null
-if ! [ -a $workdir/turnip-23.2.0-T-Alpha_MrPurple.adpkg.zip ];
-	then echo -e "$red-Packing failed!$nocolor" && exit 1
-	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip-23.0.0-T-Alpha_MrPurple.adpkg.zip
+driverzip="$workdir/turnip-$MESA_COMMIT_HASH-git.adpkg.zip"
+echo "Packing files..." $'\n'
+zip $driverzip $driverdir/*
+if ! [ -a $driverzip ];
+	then echo -e "Packing failed!" && exit 1
+	else echo -e "All done!" 
 fi
